48700cf1a8462219b9917c1c4dd51e5203f42958
Uduakobong Odudu <146853614+Ud-Repo@users.noreply.github.com>
2023-11-16 15:19:38 -0400

    jade.py

--- jade.py
+++ jade.py
@@ -1,3 +1,10 @@
-from datetime import date
 
-print (date.today())
+# step 1 : Create a list of programming languages and difficulty levels
+programming_languages = ["Python","Javascript","C++","Ruby","Java"]
+Difficulty = [2,3,4,2,5]
+
+#step 2 : combine the two lists. This is called a tuple
+
+quiz_data = list(zip(programming_languages, Difficulty))
+
+print(quiz_data)
