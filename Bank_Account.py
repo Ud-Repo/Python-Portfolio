53e7a1275fe40135298390f4f9af3105e6f68afb
Uduakobong Odudu <146853614+Ud-Repo@users.noreply.github.com>
2024-01-18 09:21:42 -0400

    d

--- Bank_Account.py
+++ Bank_Account.py
@@ -1,2 +1,11 @@
+#Create a bank account by creating a dictionary
 def create_account(owner, balance=0):
-    return{"owner":owner, "balance" : balance}
+    return {"owner":owner, "balance" : balance}
+def deposit(account, amount):
+    account["balance"] += amount
+    print (f"Deposited ${amount}. New balance : ${account['balance']}")
+    def withdraw(account, amount):
+        if amount <= account["balance"]:
+            print (f"Withdrew ${amount}. New balance : ${account['balance']}")
+ #pass [used when you dont want to put in a value yet.]
+ 
