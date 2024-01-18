2b56360212a0ef4dbfab7ebbd3ce7f22281fdee1
Uduakobong Odudu <146853614+Ud-Repo@users.noreply.github.com>
2024-01-18 10:03:04 -0400

    ..

--- Bank_Account.py
+++ Bank_Account.py
@@ -7,7 +7,7 @@ def deposit(account, amount):
     account["balance"] += amount
     print (f"Deposited ${amount}. New balance : ${account['balance']}")
     #Withdraw from the account
-    def withdraw(account, amount):
+def withdraw(account, amount):
         if amount <= account["balance"]:
             account["balance"] -= amount
             print (f"Withdrew ${amount}. New balance : ${account['balance']}")
@@ -16,7 +16,14 @@ def deposit(account, amount):
          
  #pass [used when you dont want to put in a value yet.]
  
-    def display_balance(account):
+def display_balance(account):
         print(f"Account owner: {account['owner']} , Balance : ${account['balance']}")
         
- 
+ #main
+account1 = create_account("Davis")
+#check the account
+#print(account1)
+display_balance(account1)
+deposit(account1,1000)
+withdraw(account1,500)
+print(type(account1))
