548bacfa0628dedccdfee2f1be48484c4b0545b3
Uduakobong Odudu <146853614+Ud-Repo@users.noreply.github.com>
2024-01-18 10:43:36 -0400

    .

--- Bank_Account_OOP.py
+++ Bank_Account_OOP.py
@@ -1,21 +1,22 @@
 class BankAccount:
-    #Initialize the class using the Constructor(_init_method)
-    def _init(self, owner, balance=0): 
-     self.owner = owner
-     self.balance = balance
-     
-    def deposit(self,amount):
-     self.balance += amount
-     print(f"Deposited ${amount}. New Balance: ${self.balance}")
-     
-     def withdraw (self, amount):
-         if amount <= self.balance:
-          print (f"Withdrew ${amount}. New balance : ${self.balance}")
-         else:
-             print("Insufficient funds!!")
-             
-             def display_balance(self):
-                 print (f"Account owner :{self.owner}, Balance : ${self.balance}")
-                 
+    # Initialize the class using the Constructor (__init__ method)
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited ${amount}. New Balance: ${self.balance}")
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print(f"Withdrew ${amount}. New balance: ${self.balance}")
+        else:
+            print("Insufficient funds!!")
+
+    def display_balance(self):
+        print(f"Account owner: {self.owner}, Balance: ${self.balance}")
+
 account1 = BankAccount("Davis")
 account1.display_balance()
