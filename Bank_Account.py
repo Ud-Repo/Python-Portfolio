b181c1c3471ee165ea6b27d5337280680bc4313e
Uduakobong Odudu <146853614+Ud-Repo@users.noreply.github.com>
2024-01-18 10:41:03 -0400

    .

--- Bank_Account.py
+++ Bank_Account.py
@@ -1,29 +1,29 @@
-#Create a bank account by creating a dictionary
+# Create a bank account by creating a dictionary
 def create_account(owner, balance=0):
-    return {"owner":owner, "balance" : balance}
+    return {"owner": owner, "balance": balance}
 
 # Deposit into the account
 def deposit(account, amount):
     account["balance"] += amount
-    print (f"Deposited ${amount}. New balance : ${account['balance']}")
-    #Withdraw from the account
+    print(f"Deposited ${amount}. New balance: ${account['balance']}")
+
+# Withdraw from the account
 def withdraw(account, amount):
-        if amount <= account["balance"]:
-            account["balance"] -= amount
-            print (f"Withdrew ${amount}. New balance : ${account['balance']}")
-        else:
-         print("Insufficient funds!!")
-         
- #pass [used when you dont want to put in a value yet.]
- 
+    if amount <= account["balance"]:
+        account["balance"] -= amount
+        print(f"Withdrew ${amount}. New balance: ${account['balance']}")
+    else:
+        print("Insufficient funds!!")
+
+# Pass [used when you don't want to put in a value yet.]
 def display_balance(account):
-        print(f"Account owner: {account['owner']} , Balance : ${account['balance']}")
-        
- #main
+    print(f"Account owner: {account['owner']}, Balance: ${account['balance']}")
+
+# Main
 account1 = create_account("Davis")
-#check the account
-#print(account1)
+# Check the account
+# print(account1)
 display_balance(account1)
-deposit(account1,1000)
-withdraw(account1,500)
+deposit(account1, 1000)
+withdraw(account1, 500)
 print(type(account1))
--- Bank_Account_OOP.py
+++ Bank_Account_OOP.py
@@ -1,4 +1,21 @@
 class BankAccount:
     #Initialize the class using the Constructor(_init_method)
     def _init(self, owner, balance=0): 
-     pass
+     self.owner = owner
+     self.balance = balance
+     
+    def deposit(self,amount):
+     self.balance += amount
+     print(f"Deposited ${amount}. New Balance: ${self.balance}")
+     
+     def withdraw (self, amount):
+         if amount <= self.balance:
+          print (f"Withdrew ${amount}. New balance : ${self.balance}")
+         else:
+             print("Insufficient funds!!")
+             
+             def display_balance(self):
+                 print (f"Account owner :{self.owner}, Balance : ${self.balance}")
+                 
+account1 = BankAccount("Davis")
+account1.display_balance()
